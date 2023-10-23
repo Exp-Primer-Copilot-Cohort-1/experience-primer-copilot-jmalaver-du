335a00d9755ab52ca2463be94f33cde44ea87f9d
jmalaver-du <144830239+jmalaver-du@users.noreply.github.com>
2023-10-23 16:05:56 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember () {
+    return {
+        restrict: 'E',
+        templateUrl: 'modules/skills/views/member.html',
+        contorller: 'SkillsMemberController',
+        bindToController: true,
+        scope: {
+            member: '='
+        }
+    }
+}
